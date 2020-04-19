1cd03eb7ea04718dc4b27cdaab3d119265e9375a
Philipp Dufter <philipp.dufter@gmail.com>
2020-04-19 18:57:30 +0200

    add setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# install npm
+# install python dependencies with requirements txt
+
+# install bower
+npm install -g bower
+
+
+# install js dependencies
+install bower install bootstrap#3.2.0 jquery#2.1.1 d3#3.4.11 --save
+
+
