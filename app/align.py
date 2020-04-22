985e53f19342c6660ad0f5f0a75daaa93dbc0292
Philipp Dufter <philipp.dufter@gmail.com>
2020-04-22 12:11:47 +0200

    adjust alignment format

--- app/align.py
+++ app/align.py
@@ -14,6 +14,14 @@ print("SETTING UP ALIGNMENT MODELS")
 aligner = simalign.SentenceAligner(model="bert", token_type="bpe")
 
 
+def convert_alignment(initial_output):
+    result = []
+    for elem in initial_output:
+        i, j = elem.split("-")
+        result.append([int(i), int(j)])
+    return result
+
+
 class LoginForm(FlaskForm):
     english = StringField('Sentence A:', validators=[DataRequired()], default="")
     foreign = StringField('Sentence B:', validators=[DataRequired()])
@@ -31,9 +39,11 @@ def index():
         #     form.username.data, form.remember_me.data))
         res = aligner.get_word_aligns([form.english.data.split(" "), form.foreign.data.split(" ")])
         print(res)
+        print(convert_alignment(res['itermax']))
+        #{'mwmf': ['0-0', '1-1'], 'inter': ['0-0', '1-1'], 'itermax': ['0-0', '1-1']}
         alignment = {"e": form.english.data.split(" "),
                 "f": form.foreign.data.split(" "), 
                 #"alignment": [[i, i] for i in range(min(len(form.english.data.split(" ")), len(form.foreign.data.split(" "))))]}
-                "alignment": res}
+                "alignment": convert_alignment(res['itermax'])}
         return render_template('index.html', title='SimAlign', form=form, alignment=alignment)
     return render_template('index.html', title='SimAlign', form=form, alignment=alignment)
