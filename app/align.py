4312df4b90d12020f053653c6d3cd3cfdc488ff8
Philipp Dufter <philipp.dufter@gmail.com>
2020-04-22 12:08:07 +0200

    integrate alignemnt tool

--- app/align.py
+++ app/align.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, jsonify
 from app import app
-# import sys
-# sys.path.append("/Users/philipp/Dropbox/Inbox/simalign")
-# import simalign
+import sys
+sys.path.append("/mounts/work/philipp/simalign-demo/simalign")
+import simalign
 #from app.forms import LoginForm
 
 from flask_wtf import FlaskForm, RecaptchaField
@@ -11,7 +11,7 @@ from wtforms.validators import DataRequired, Length, ValidationError
 
 # setting up alignment models
 print("SETTING UP ALIGNMENT MODELS")
-#aligner = simalign.SentenceAligner(model="bert", token_type="bpe")
+aligner = simalign.SentenceAligner(model="bert", token_type="bpe")
 
 
 class LoginForm(FlaskForm):
@@ -29,10 +29,11 @@ def index():
     if form.validate_on_submit():
         # flash('Login requested for user {}, remember_me={}'.format(
         #     form.username.data, form.remember_me.data))
-        #res = aligner.get_word_aligns([form.english.data.split(" "), form.foreign.data.split(" ")])
+        res = aligner.get_word_aligns([form.english.data.split(" "), form.foreign.data.split(" ")])
         #print(res)
         alignment = {"e": form.english.data.split(" "),
                 "f": form.foreign.data.split(" "), 
-                "alignment": [[i, i] for i in range(min(len(form.english.data.split(" ")), len(form.foreign.data.split(" "))))]}
+                #"alignment": [[i, i] for i in range(min(len(form.english.data.split(" ")), len(form.foreign.data.split(" "))))]}
+                "alignment": res}
         return render_template('index.html', title='SimAlign', form=form, alignment=alignment)
     return render_template('index.html', title='SimAlign', form=form, alignment=alignment)
