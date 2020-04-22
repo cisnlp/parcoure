b7818399e477ab945584f362550a5be998382c67
Philipp Dufter <philipp.dufter@gmail.com>
2020-04-22 10:13:34 +0200

    integrate library

--- app/align.py
+++ app/align.py
@@ -1,5 +1,8 @@
 from flask import render_template, flash, redirect, jsonify
 from app import app
+#import sys
+#sys.path.append("/Users/philipp/Dropbox/Inbox/simalign")
+#import simalign
 #from app.forms import LoginForm
 
 from flask_wtf import FlaskForm, RecaptchaField
@@ -17,11 +20,14 @@ class LoginForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    #aligner = simalign.SentenceAligner(model="bert", token_type="bpe")
     form = LoginForm()
     alignment = None
     if form.validate_on_submit():
         # flash('Login requested for user {}, remember_me={}'.format(
         #     form.username.data, form.remember_me.data))
+        #res = aligner.get_word_aligns([form.english.data.split(" "), form.foreign.data.split(" ")])
+        #print(res)
         alignment = {"e": form.english.data.split(" "),
                 "f": form.foreign.data.split(" "), 
                 "alignment": [[i, i] for i in range(min(len(form.english.data.split(" ")), len(form.foreign.data.split(" "))))]}
